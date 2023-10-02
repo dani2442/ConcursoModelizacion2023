71d501fb76125b8cd89abbd24acbeb35a48b67aa
Antimateri <pablo.cageaoh@gmail.com>
2023-10-02 14:34:27 +0200

    limpiar un poco

--- heuristicas.py
+++ heuristicas.py
@@ -1,23 +1,23 @@
 from gradient_descent import Environment
 import numpy as np
 
-#Tengo que formalizar que esto es cota minima
+# Acotación de impacto
 def estrellato(env: Environment, Fmax: int) -> int:
-    p = env.p_0
+    p = np.copy(env.p_0)
     m = env.m
-    v = env.v_0
-    k=0;
+    v = np.copy(env.v_0)
+    a = np.array([(-pa)/abs(pa)*Fmax/m for pa in p])
+    a[2] -= env.g*m
+    k=0
     visto = [False] * 3
     while(True):
         if(visto[0] and visto[1] and visto[2]):
             break
         k+=1
-        a = np.array([(-pa)/abs(pa)*Fmax/m for pa in p])
-        a[3] -= env.g*m
-        pre_p=p
+        pre_p=np.copy(p)
         p+=v*env.dt + a*env.dt**2/2
         v+=a*env.dt
-        c=pre_p*p
+        c=[pre_p[0]*p[0],pre_p[1]*p[1],pre_p[2]*p[2]]
         if(c[0]<=0):
             visto[0]=True
         if(c[1]<=0):
